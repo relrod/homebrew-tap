32f0f3486e947deb95587d255e4f321788af69ff
Rick Elrod <rick@elrod.me>
2021-03-24 03:22:27 -0500

    init
    Signed-off-by: Rick Elrod <rick@elrod.me>

---
+++ Formula/sshpass.rb
@@ -0,0 +1,20 @@
+class Sshpass < Formula
+  desc "Non-interactive SSH password auth"
+  homepage "https://sourceforge.net/projects/sshpass/"
+  url "https://downloads.sourceforge.net/project/sshpass/sshpass/1.09/sshpass-1.09.tar.gz"
+  sha256 "71746e5e057ffe9b00b44ac40453bf47091930cba96bbea8dc48717dedc49fb7"
+
+  def install
+    system "./configure",
+           "--disable-debug",
+           "--disable-dependency-tracking",
+           "--disable-silent-rules",
+           "--prefix=#{prefix}"
+    system "make", "install"
+    man1.install "sshpass.1"
+  end
+
+  test do
+    system "sshpass"
+  end
+end
---
+++ README.md
@@ -0,0 +1,11 @@
+# relrod Tap
+
+## How do I install these formulae?
+
+`brew install relrod/tap/<formula>`
+
+Or `brew tap relrod/tap` and then `brew install <formula>`.
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
